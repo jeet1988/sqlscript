b00be57abae80af951b156d9a762269d40f45030
jeet1988 <43564736+jeet1988@users.noreply.github.com>
2018-12-11 11:34:46 +0530

    multi transation script

---
+++ V1.7__Mysqltable.sql
@@ -0,0 +1,39 @@
+-- 1. start a new transaction
+START TRANSACTION;
+ 
+-- 2. create DB
+CREATE DATABASE SampleCICDDB;
+
+-- 3 .Cretete table
+
+ CREATE TABLE SampleCICDDB.CUSTOMERS(
+   ID   INT              NOT NULL,
+   NAME VARCHAR (20)     NOT NULL,
+   AGE  INT              NOT NULL,
+   ADDRESS  CHAR (25) ,
+   SALARY   DECIMAL (18, 2),       
+   PRIMARY KEY (ID)
+);
+ 
+-- 3. insert a new order for customer 145
+INSERT INTO SampleCICDDB.CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (1, 'Ramesh', 32, 'Ahmedabad', 2000.00 );
+
+INSERT INTO SampleCICDDB.CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (2, 'Khilan', 25, 'Delhi', 1500.00 );
+
+INSERT INTO SampleCICDDB.CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (3, 'kaushik', 23, 'Kota', 2000.00 );
+
+INSERT INTO SampleCICDDB.CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (4, 'Chaitali', 25, 'Mumbai', 6500.00 );
+
+INSERT INTO SampleCICDDB.CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (5, 'Hardik', 27, 'Bhopal', 8500.00 );
+
+INSERT INTO SampleCICDDB.CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (6, 'Komal', 22, 'MP', 4500.00 );
+
+      -- 5. commit changes   
+-- rollback 
+COMMIT;
